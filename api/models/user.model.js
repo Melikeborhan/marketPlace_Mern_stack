b84184fa16a9fcfed3d1cda8b03cc30360c98260
Melike <22410051025@ogr.erbakan.edu.tr>
2024-07-17 17:18:48 +0300

    connect to database and create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        typeof: String,
+        required: true,
+        unique:true,
+},
+    email: {
+        typeof: String,
+        required: true,
+        unique:true,
+},
+    password: {
+        typeof: String,
+        required: true,
+    }
+       
+}, { timestamps: true }); // this will add createdAt and updatedAt timestamps
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
