2677eab87c421dfd2223b99d6333ef5ecd72c5ca
Melike <22410051025@ogr.erbakan.edu.tr>
2024-08-04 19:35:49 +0300

    profile page UI edit

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 export default function Profile() {
+  const { currentUser } = useSelector(state => state.user)
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <h1 className='text-4xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
+        <img src={currentUser.avatar}  alt='profile' className='w-32 h-32  cursor-pointer self-center rounded-full object-cover'/>
         <input 
         type='text'
         placeholder='username'
@@ -33,6 +36,10 @@ export default function Profile() {
              Create Listing
       </Link>
     </form>
+    <div className='flex justify-between mt-5'>
+    <span className='text-red-700 cursor-pointer font-bold'>Delete Account</span>
+    <span className='text-red-700 cursor-pointer font-bold'>Sign Out</span>
+    </div>
      
     
     </div>
